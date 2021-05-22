6ba0b1daa0e46ad82b9a068ff442b39fcebaa5f9
soonniee <ckstns1231@gmail.com>
2021-05-23 01:59:39 +0900

    Detail Page 구조

---
+++ src/components/detail/Comment.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Comment = () => {
+  return <CommentWrapper></CommentWrapper>;
+};
+
+export default Comment;
+const CommentWrapper = styled.div``;
