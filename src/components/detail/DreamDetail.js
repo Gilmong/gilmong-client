08695151b9a1828864ae53a28d3cfa8b4a4c0a72
soonniee <ckstns1231@gmail.com>
2021-05-23 01:59:39 +0900

    Detail Page 구조

---
+++ src/components/detail/DreamDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const DreamDetail = () => {
+  return <DreamDetailWrapper></DreamDetailWrapper>;
+};
+
+export default DreamDetail;
+const DreamDetailWrapper = styled.div``;
---
+++ src/pages/DetailPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import DetailHeader from "../components/detail/DetailHeader";
+import DreamDetail from "../components/detail/DreamDetail";
+import Comment from "../components/detail/Comment";
+
+const DetailPage = () => {
+  return (
+    <DetailPageWrapper>
+      <DetailHeader />
+      <DreamDetail />
+      <Comment />
+    </DetailPageWrapper>
+  );
+};
+
+export default DetailPage;
+const DetailPageWrapper = styled.div``;
