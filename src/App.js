c3c7301ead2ce538298fc65a3c392a1165aacb1f
JungHyun <leahincom@gmail.com>
2021-05-23 01:20:40 +0900

    update Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import MainPage from "./pages/MainPage";
+import MyPage from "./pages/MyPage";
+import DetailPage from "./pages/DetailPage";
+import UploadPage from "./pages/UploadPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route
+          path="/mypage/:id"
+          breadcrumb="MyPage"
+          component={(props) => <MyPage userId={props.match.params.id} />}
+        />
+        <Route
+          path="/upload/:id"
+          breadcrumb="Upload"
+          component={(props) => <UploadPage dreamId={props.match.params.id} />}
+        />
+        <Route
+          path="/detail/:id"
+          breadcrumb="Detail"
+          component={(props) => <DetailPage userId={props.match.params.id} />}
+        />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
